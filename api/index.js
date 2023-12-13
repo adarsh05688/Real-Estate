402ee2241487304e4ddc123aba82471822debc87
prins mishra <mishraprins@outlook.com>
2023-12-13 13:21:50 +0530

    rep created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import UserRouter from './routes/user.route.js';
+import AuthRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.CONNECTION).then(()=>{
@@ -11,7 +13,10 @@ mongoose.connect(process.env.CONNECTION).then(()=>{
 
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000, ()=>{
     console.log("app is running on port 3000.....");
 });
+
+app.use('/api/user' , UserRouter);
+app.use('/api/auth', AuthRouter)
