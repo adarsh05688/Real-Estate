df217b1286340c575afc491389c700861de67202
prins mishra <mishraprins@outlook.com>
2023-12-13 14:10:19 +0530

    created middleware

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js';
 import AuthRouter from './routes/auth.route.js'
+ 
 dotenv.config();
 
 mongoose.connect(process.env.CONNECTION).then(()=>{
@@ -14,9 +15,20 @@ mongoose.connect(process.env.CONNECTION).then(()=>{
 
 const app = express();
 app.use(express.json());
+ 
 app.listen(3000, ()=>{
     console.log("app is running on port 3000.....");
 });
 
 app.use('/api/user' , UserRouter);
-app.use('/api/auth', AuthRouter)
+app.use('/api/auth', AuthRouter);
+app.use((err,req,res,next)=>{
+    const statusCode  = err.statusCode || 500;
+    const message = err.message || 'Internal server Error';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message,
+    })
+
+});
