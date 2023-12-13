ce83c1a969c15ecce35c2ae0c19dad3091c04bbd
prins mishra <mishraprins@outlook.com>
2023-12-13 11:45:47 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,20 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from './routes/user.route.js'
 import dotenv from 'dotenv';
 dotenv.config();
- 
- 
-mongoose.connect("mongodb+srv://adarsh056880:12345@mern-estate.w2p0gjl.mongodb.net/?retryWrites=true&w=majority").then(()=>{
-    console.log('connected to mongodb');
+
+mongoose.connect(process.env.CONNECTION).then(()=>{
+    console.log("connected to database");
 }).catch((err)=>{
     console.log(err);
 });
- 
+
+
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("server is running in port 3000....");
-})
-
-app.use('/api/user', userRouter);
+app.listen(3000, ()=>{
+    console.log("app is running on port 3000.....");
+});
